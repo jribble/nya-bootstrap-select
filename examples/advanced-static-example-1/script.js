6d364ee109c73aa639a4fa6a39f05b5326a0f476
Jarrod Ribble <jarrod.ribble@gmail.com>
2015-08-04 16:30:16 -0600

    Updates

--- examples/advanced-static-example-1/script.js
+++ examples/advanced-static-example-1/script.js
@@ -3,7 +3,7 @@
   var myApp = angular.module('AdvancedStaticExample1App', ['nya.bootstrap.select']);
 
   myApp.controller('MainController', ['$scope', function($scope){
-  
-
+  $script.interval=1;
+$script.units='WEEK';
   }]);
 })(window.angular);
